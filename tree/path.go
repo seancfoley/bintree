d001d70871256e32c5faf3fce305ca680bb14f6a
Sean Foley <scfoley@us.ibm.com>
2024-01-27 01:40:26 -0500

    minor fix to size calc for paths

--- tree/path.go
+++ tree/path.go
@@ -133,18 +133,18 @@ func (node *PathNode[E, V]) Size() (storedSize int) {
 			for ; next != nil && next.storedSize == sizeUnknown; prev, next = next, next.next {
 			}
 			var nodeSize int
+			if next != nil {
+				nodeSize = next.storedSize
+			}
 			for {
 				if prev.IsAdded() {
 					nodeSize++
-				}
-				if next != nil {
-					nodeSize += next.storedSize
 				}
 				prev.storedSize = nodeSize
 				if prev == node {
 					break
 				}
-				prev = node.previous
+				prev = prev.previous
 			}
 			storedSize = node.storedSize
 		}
@@ -198,10 +198,10 @@ func (node *PathNode[E, V]) printList(builder *strings.Builder,
 			builder.WriteString(nonAddedNodeCircle)
 			builder.WriteByte('\n')
 		}
-		indents.nodeIndent = indents.subNodeInd + rightElbow
-		indents.subNodeInd = indents.subNodeInd + belowElbows
 		if next = next.next; next == nil {
 			break
 		}
+		indents.nodeIndent = indents.subNodeInd + rightElbow
+		indents.subNodeInd += belowElbows
 	}
 }
